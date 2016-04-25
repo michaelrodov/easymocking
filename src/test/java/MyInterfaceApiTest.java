c4cf559d7e5456639e6be299f212d9ad5026d075
Michael Rodov <rodov@hpe.com>
2016-04-25 16:24:18 +0300

    play

--- src/test/java/MyInterfaceApiTest.java
+++ src/test/java/MyInterfaceApiTest.java
@@ -15,14 +15,28 @@ public class MyInterfaceApiTest extends EasyMockSupport {
     private MyInterfaceApi testedClass = new MyInterfaceApi(); //step 2 for runner
 
     /*assign mock impl ONLY for myInterface2 member of tested object*/
-    @Mock(type= MockType.NICE, name="myInterfaceNice", fieldName = "myInterface2")
-    private MyInterface myInterfaceMock; //step 1 for runner
+    @Mock(type= MockType.STRICT, name="myInterfaceNice", fieldName = "myInterface2")
+    private MyInterfaceImpl myInterfaceMock; //step 1 for runner
 
     @Test
     public void doSomethingTest(){
         //prepare all for usage
+
+        /**define full class mock*/
+        //EasyMock.expect(myInterfaceMock.getMeString()).andReturn("[MOCK]");
+
+        /**define partial class mock*/
+        myInterfaceMock = EasyMock.createMockBuilder(MyInterfaceImpl.class).addMockedMethod("getMeInt",Double.class).createMock();
+        EasyMock.expect(myInterfaceMock.getMeInt(EasyMock.anyDouble())).andReturn(3333);
+
+        //activate mocks
         replayAll();
+        //run the tested function
         testedClass.doSomething("TEST1", 1.0);
+        //validate that all mocks were used
+        verifyAll();
+        //assert results
+        //TODO assert
     }
 
 }
